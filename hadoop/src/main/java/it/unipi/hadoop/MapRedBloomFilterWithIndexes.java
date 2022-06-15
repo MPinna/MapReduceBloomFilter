21b0bd59bb3ee91fdfe70b654cf5c42726a18509
Matteo Biondi <matteobiondi1197@gmail.com>
2022-06-15 15:31:05 +0200

    Update MapRedBloomFilterWithIndexes.java
    Implemented Mapper with indexes and Main [WIP]

--- hadoop/src/main/java/it/unipi/hadoop/MapRedBloomFilterWithIndexes.java
+++ hadoop/src/main/java/it/unipi/hadoop/MapRedBloomFilterWithIndexes.java
@@ -5,24 +5,89 @@ import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayPrimitiveWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.hash.MurmurHash;
+
+import com.google.common.hash.HashFunction;
 
 public class MapRedBloomFilterWithIndexes
 {
                                                         
-    public static class MapRedBloomFilterMapper extends Mapper<, , , > // change parameters type
+    public static class MapRedBloomFilterMapper extends Mapper<Object, Text, IntWritable, ArrayPrimitiveWritable> 
     {
-        //TODO add setup and clean for classes
+        // Number of hash functions to be computed
+        private static int k;
+
+        //Reuse writable key and value obj
+        private static final IntWritable rate = new IntWritable();
+        private static final ArrayPrimitiveWritable hashesValue = new ArrayPrimitiveWritable();
         
+        //Utility objs
+        private final static MurmurHash murmurHash = (MurmurHash) MurmurHash.getInstance();
+        private static int[] hashValue;
+
+        public void setup(Context context) throws IOException, InterruptedException
+        {
+            // Set parameters (from job configiguration)
+            k =  context.getConfiguration().getInt("k_param", 5);
+            // Utility data structure
+            hashValue = new int[k];
+        }
+
         public void map(final Object key, final Text value, final Context context)
                 throws IOException, InterruptedException {
             
+                    // Get input values and check correctness in length and type
+                    String record = value.toString();
+                    if (record == null || record.length() == 0)
+                        return;
+
+                    String[] tokens = record.trim().split("\\t");
+
+                    if (tokens.length == 3) {
+                        String movieId = null;
+                        double rawRate;
+
+                        movieId= tokens[0];
+                        if(movieId == null){
+                            return;
+                        }
+
+                        try{
+                            rawRate = Double.parseDouble(tokens[1]);
+                        }
+                        catch(NumberFormatException e){
+                            return;
+                        }
+                        if (rawRate < 1.0 || rawRate > 10.0){
+                            return;
+                        }
+                        
+                        // Compute k hash functions
+                        for (int i = 0; i < k; i++) {
+                            hashValue[i] = murmurHash.hash(movieId.getBytes(), movieId.length(), i);
+                        }
+
+                        // Compute rounded rating and set Map key
+                        rate.set((int)Math.ceil(rawRate));
+
+                        // Set Map value
+                        hashesValue.set(hashValue);
+
+                        // Emit rating e values of the k hash functions
+                        context.write(rate, hashesValue);
+                    }
         }
     }
 
@@ -43,11 +108,11 @@ public class MapRedBloomFilterWithIndexes
                     //Add indexes
                     for(List<IntWritable> value: values){
                         for(IntWritable index: value){
-                            bloomfilter.setAt(index.get())
+                            bloomfilter.setAt(index.get());
                         }
                     }
 
-                    context.write(null, bloomfilter)
+                    context.write(null, bloomfilter);
         }
     }
 
@@ -55,18 +120,48 @@ public class MapRedBloomFilterWithIndexes
 
 
         final Configuration conf = new Configuration();
-        final Job job = new Job(conf, "bloomfilter");
+        final Job job = new Job(conf, "BloomFilter");
         job.setJarByClass(MapRedBloomFilterWithIndexes.class);
 
-        //TODO To change
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+           System.err.println("Usage: MatrixMultiplication <input> <output>");
+           System.exit(1);
+        }
+        System.out.println("args[0]: <input>="  + otherArgs[0]);
+        System.out.println("args[1]: <output>=" + otherArgs[1]);
 
+        //TODO To change
+        //Set the BloomFilter parameters
+        job.getConfiguration().set("k_param", "5");
+        job.getConfiguration().set("m_param", "1000000");
+        job.getConfiguration().set("p_param", "0.001");
+
+
+        //Set mapper and reducer
         job.setMapperClass(MapRedBloomFilterMapper.class);
         job.setReducerClass(MapRedBloomFilterReducer.class);
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        //TODO To check
+        //Define type of mapper's key-value output pair
+        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputValueClass(ArrayPrimitiveWritable.class);
+        
+        //Define type of reducer's key-value output pair
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(BloomFilter.class);
+
+        //TODO to change
+        //Define number of reduce tasks
+        job.setNumReduceTasks(1);
+        
+        //Define I/O
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
+        //TODO to check
+        job.setInputFormatClass(NLineInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
