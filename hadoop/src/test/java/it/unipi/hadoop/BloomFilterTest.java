95c0ce7206e1dfadc88a184b90e5feac6bd6056f
Federico Cristofani <84182920+federico-cristofani@users.noreply.github.com>
2022-06-15 18:34:04 +0200

    BloomFilter test class

---
+++ hadoop/src/test/java/it/unipi/hadoop/BloomFilterTest.java
@@ -0,0 +1,30 @@
+package it.unipi.hadoop;
+
+/*import org.junit.Test;
+import static org.junit.Assert.*;*/
+
+public class BloomFilterTest{
+    
+    private final int M = 1 << 23; // 1 MB = 2 ^ 20 bit
+    private final int K = 3;
+    private final float P = 0.001f;
+
+    private final int N_ADD = 1000;
+    private final int N_TEST = 1000;
+
+    private BloomFilter bloomFilter;
+
+    //@Test  
+    public void test() {
+        for (int rating = 1; rating < 11; ++rating){
+            bloomFilter = new BloomFilter(rating, M, K, P);
+            
+            for (int i = 0; i < N_ADD; ++i)
+                bloomFilter.add(String.format("ID_%d", i));
+            
+            for (int i = 0; i < N_TEST; ++i)
+                //assertTrue(bloomFilter.test(String.format("ID_%d", i)));
+                assert(bloomFilter.test(String.format("ID_%d", i)));
+        }
+    }
+}
