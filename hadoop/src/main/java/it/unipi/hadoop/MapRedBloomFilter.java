d5ec1de5a781991b0a18d317b3aad5813859a574
Federico Cristofani <84182920+federico-cristofani@users.noreply.github.com>
2022-06-20 12:49:13 +0200

    merge versions

--- hadoop/src/main/java/it/unipi/hadoop/MapRedBloomFilter.java
+++ hadoop/src/main/java/it/unipi/hadoop/MapRedBloomFilter.java
@@ -3,7 +3,9 @@ import java.io.IOException;
 import org.apache.log4j.Logger;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayPrimitiveWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.Job;
@@ -16,32 +18,30 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class MapRedBloomFilter
 {
-    public static class MapRedBloomFilterMapper extends Mapper<Object, Text, IntWritable, BloomFilter>
+    public static class MapRedBloomFilterMapperWithBloomFilters extends Mapper<Object, Text, IntWritable, BloomFilter>
     {
-        private final static int N_RATINGS = 10;
-        private static float P;
-        private static int M;
-        private static int K;
+        private static int[] m;
+        private static int k;
         
         private static BloomFilter[] bloomFilters;
-        private static IntWritable outKey = new IntWritable();
+        private static IntWritable rateKey = new IntWritable();
         private static Logger logger;
 
         @Override
         protected void setup(Context context) throws IOException, InterruptedException {
-            logger = Logger.getLogger(MapRedBloomFilterMapper.class.getName());
+            logger = Logger.getLogger(MapRedBloomFilterMapperWithBloomFilters.class.getName());
+            m = new int[UtilityConstants.NUM_OF_RATES];
             
             // Set parameters (from job configuration)
-            K =  context.getConfiguration().getInt("k_param", 5);
-            M =  context.getConfiguration().getInt("m_param", 8192);
-            P =  context.getConfiguration().getFloat("p_param", 0.01f);
-            
-            logger.info(String.format("Received values: <M = %d K = %d P = %f>", M, K, P));
-            
+            k = context.getConfiguration().getInt("k_param", UtilityConstants.DEFAULT_K);
+            for (int i = 0; i < UtilityConstants.NUM_OF_RATES; ++i)
+                m[i] = context.getConfiguration().getInt("m_param_rate_"+(i+1), UtilityConstants.DEFAULT_M);
+                        
             // Init bloom filters
-            bloomFilters = new BloomFilter[N_RATINGS];
-            for (int i = 0; i < N_RATINGS; ++i){
-                bloomFilters[i] = new BloomFilter(i+1, M, K, P);
+            bloomFilters = new BloomFilter[UtilityConstants.NUM_OF_RATES];
+            for (int i = 0; i < UtilityConstants.NUM_OF_RATES; ++i){
+                bloomFilters[i] = new BloomFilter(i+1, m[i], k, -1);
+                logger.info(String.format("BloomFilter[%d]: <M = %d K = %d>", i+1, m[i], k));
             }
         }
     
@@ -49,7 +49,7 @@ public class MapRedBloomFilter
         public void map(Object key, Text value, Context context) 
                 throws IOException, InterruptedException 
         {
-            // Get raw input values
+            /*// Get raw input values
             String tokens[] = value.toString().split("\t+");
             
             // Check format (last value is discarded)
@@ -76,42 +76,55 @@ public class MapRedBloomFilter
             }
             catch(NumberFormatException e){
                 return;
+            }*/
+
+            Object[] tokens = Util.parseInput(value.toString());  
+            if (tokens == null){
+                logger.info(String.format("Invalid entry: %s", value.toString()));
+                return;  
             }
-            
+ 
             // Update bloom filter
-            bloomFilters[index-1].add(movieId);            
+            bloomFilters[(int)tokens[1]-1].add((String)tokens[0]);            
         }
 
         /*
          * The 10 bloom filters are emitted just once at the end of map task
          */
         protected void cleanup(Context context) throws IOException, InterruptedException {
-            for (int i = 0; i < N_RATINGS; ++i){
-                outKey.set(i + 1);
-                context.write(outKey,  bloomFilters[i]);
+            for (int i = 0; i < UtilityConstants.NUM_OF_RATES; ++i){
+                rateKey.set(i + 1);
+                context.write(rateKey,  bloomFilters[i]);
             }
         }
 
     }
     
-    public static class MapRedBloomFilterReducer extends Reducer<IntWritable, BloomFilter, IntWritable, BloomFilter> {
+    public static class MapRedBloomFilterReducerWithBloomFilters extends Reducer<IntWritable, BloomFilter, IntWritable, BloomFilter> {
 
         private static int k;
-        private static int m;
-        private static float p;
+        private static int[] m;
+        private static Logger logger;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            logger = Logger.getLogger(MapRedBloomFilterReducerWithBloomFilters.class.getName());
+            m = new int[UtilityConstants.NUM_OF_RATES];
+            
+            // Set parameters (from job configuration)
+            k = context.getConfiguration().getInt("k_param", UtilityConstants.DEFAULT_K);
+            for (int i = 0; i < UtilityConstants.NUM_OF_RATES; ++i)
+                m[i] = context.getConfiguration().getInt("m_param_rate_"+(i+1), UtilityConstants.DEFAULT_M);
+        }
 
         @Override
         public void reduce(IntWritable key, Iterable<BloomFilter> values, final Context context)
                 throws IOException, InterruptedException 
         {
-
-
-            k = context.getConfiguration().getInt("k_param", 5);
-            m = context.getConfiguration().getInt("m_param", 8192); // 5MiB
-            p = context.getConfiguration().getFloat("p_param", (float) 0.001);
-
             int rating = key.get();
-            BloomFilter bloomFilter = new BloomFilter(rating, m, k, p);
+            BloomFilter bloomFilter = new BloomFilter(rating, m[rating-1], k, -1);
+            
+            logger.info(String.format("BloomFilter[%d]: <M = %d K = %d>", rating, m[rating-1], k));
 
             for (BloomFilter value : values) {
                 bloomFilter.or(value);
@@ -121,47 +134,206 @@ public class MapRedBloomFilter
         }
     }
 
+    public static class MapRedBloomFilterMapperWithIndexes extends Mapper<Object, Text, IntWritable, ArrayPrimitiveWritable> 
+    {
+        // Number of hash functions to be computed
+        private static int k;
+        // Vector containg the dimensions in bit of each BloomFilter
+        private static final int[] m = new int[UtilityConstants.NUM_OF_RATES];
+
+        //Reuse writable key and value obj
+        private static final IntWritable rate = new IntWritable();
+        private static final ArrayPrimitiveWritable hashesValue = new ArrayPrimitiveWritable();
+
+        public void setup(Context context) throws IOException, InterruptedException
+        {
+            // Set k and m BloomFilter parameters (from job configuration)
+            k = context.getConfiguration().getInt("k_param", UtilityConstants.DEFAULT_K);
+            for (int i = 0; i< UtilityConstants.NUM_OF_RATES; ++i)
+                m[i] = context.getConfiguration().getInt("m_param_rate_"+(i+1), UtilityConstants.DEFAULT_M);
+        }
+
+        public void map(final Object key, final Text value, final Context context)
+                throws IOException, InterruptedException {
+            
+                    // Get input values and check correctness in length and type
+                    String record = value.toString();
+                    if (record == null || record.length() == 0)
+                        return;
+
+                    String[] tokens = record.split("\t+");
+
+                    if (tokens.length == 3) {
+                        String movieId = null;
+                        float rawRate;
+
+                        movieId= tokens[0];
+                        if(movieId == null){
+                            return;
+                        }
+
+                        try{
+                            rawRate = Float.parseFloat(tokens[1]);
+                        }
+                        catch(NumberFormatException e){
+                            return;
+                        }
+                        if (rawRate < 1.0 || rawRate > 10.0){
+                            return;
+                        }
+                        
+                        // Compute rounded rate
+                        int roundedRate = Math.round(rawRate);
+
+                        // Compute k hash functions
+                        int[] hashValue = BloomFilter.computeHash(k, movieId, m[roundedRate - 1]);
+
+                        // Set Map key
+                        rate.set(roundedRate);
+
+                        // Set Map value
+                        hashesValue.set(hashValue);
+
+                        // Emit rating e values of the k hash functions
+                        context.write(rate, hashesValue);
+                    }
+        }
+    }
+
+    public static class MapRedBloomFilterReducerWithIndexes extends Reducer<IntWritable,ArrayPrimitiveWritable,NullWritable,BloomFilter> {
+
+        //TODO add clean for classes
+        // Number of hash functions to be computed
+        private static int k;
+        // Vector containg the dimensions in bit of each BloomFilter
+        private static final int[] m = new int[UtilityConstants.NUM_OF_RATES];
+        
+        // Utility attribute for code readability
+        private static final float p = UtilityConstants.FALSE_POSITIVE_RATE;
+
+        private BloomFilter bloomFilter;
+
+        public void setup(Context context) throws IOException, InterruptedException
+        {
+            // Set k and m BloomFilter parameters (from job configuration)
+            k = context.getConfiguration().getInt("k_param", UtilityConstants.DEFAULT_K);
+            for (int i = 0; i< UtilityConstants.NUM_OF_RATES; ++i)
+                m[i] = context.getConfiguration().getInt("m_param_rate_"+(i+1), UtilityConstants.DEFAULT_M);
+        }
+
+        public void reduce(final IntWritable key, final Iterable<ArrayPrimitiveWritable> values, final Context context)
+                throws IOException, InterruptedException {
+                    //Create BloomFilter for the given rating
+                    int rating = key.get();
+                    bloomFilter = new BloomFilter(rating, m[rating - 1], k, p);
+                   
+                    //Add indexes
+                    for(ArrayPrimitiveWritable value: values){
+                        int[] indexes = (int[]) value.get();
+
+                        for(int i=0; i<indexes.length; i++){
+                            bloomFilter.setAt(indexes[i]);
+                        }
+                    }
+
+                    context.write(null, bloomFilter);
+        }
+    }
+
     public static void main(final String[] args) throws Exception {
 
         final Configuration conf = new Configuration();
-        final Job job = new Job(conf, "bloomfilter");
+        final Job job = new Job(conf, "BloomFilter");
         
         job.setJarByClass(MapRedBloomFilter.class);
 
-        // Read job parameters 
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-        if (otherArgs.length != 2) {
-           System.err.println("Usage: BloomFilter <input> <output>");
+        if (otherArgs.length != 15) {
+           System.err.println("Usage: BloomFilter <input> <output> <num_lines_per_split> <m_value>{10 times} <k_value> <version>");//TODO 10 times or 10 ?
            System.exit(1);
         }
-
+        //Print input and output file path
         System.out.println("args[0]: <input>="  + otherArgs[0]);
         System.out.println("args[1]: <output>=" + otherArgs[1]);
 
-        //TODO To change (from command line ?)
+        // Get invocation parameters
+        int numLinesPerSplit = 0;
+        int[] m_value = new int[UtilityConstants.NUM_OF_RATES];
+        int k_value = 0;
+
+        try{
+            //Take num_lines_per_split parameter
+            numLinesPerSplit = Integer.parseInt(otherArgs[2]);
+            
+            //Take ten values of m parameter
+            for(short i = 0; i<UtilityConstants.NUM_OF_RATES; ++i)
+                m_value[i] = Integer.parseInt(otherArgs[3+i]);
+            
+            //Take k value
+            k_value = Integer.parseInt(otherArgs[13]);
+        }
+        catch(NumberFormatException e){
+            e.printStackTrace();
+            System.err.println("Usage: BloomFilter <input> <output> <num_lines_per_split> <m_value>{10} <k_value> <version>");
+            System.exit(1);
+        }
+        //Take implementation version to be executed
+        String version = otherArgs[14];
+        if(!version.equalsIgnoreCase(UtilityConstants.NAME_OF_VERSIONS[0]) &&  
+            !version.equalsIgnoreCase(UtilityConstants.NAME_OF_VERSIONS[1])){
+                System.err.println("Invalide <version> parameter. Options:"+
+                    "\n1)"+UtilityConstants.NAME_OF_VERSIONS[0]+
+                    "\n2)"+UtilityConstants.NAME_OF_VERSIONS[1]);
+                System.exit(1);
+            }
+
         //Set the BloomFilter parameters
-        job.getConfiguration().set("k_param", "5");
-        job.getConfiguration().set("m_param", "8192"); // 5 MiB
-        job.getConfiguration().set("p_param", "0.001");
-        
-        // Config mapper 
-        job.setMapperClass(MapRedBloomFilterMapper.class);
-        job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(BloomFilter.class);
-    
-        //Config reducer
-        job.setNumReduceTasks(1); //TODO to change
-        job.setReducerClass(MapRedBloomFilterReducer.class);
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(BloomFilter.class);
        
+        //K value
+        job.getConfiguration().set("k_param", String.valueOf(k_value));
+        
+        //M value for each rating
+        for(short i = 0; i<UtilityConstants.NUM_OF_RATES; ++i)
+            job.getConfiguration().set("m_param_rate_"+(i+1), String.valueOf(m_value[i]));
+       
+        if (version.equalsIgnoreCase(UtilityConstants.NAME_OF_VERSIONS[0])){ // WithIndexes
+            //Set mapper and reducer
+            job.setMapperClass(MapRedBloomFilterMapperWithIndexes.class);
+            job.setReducerClass(MapRedBloomFilterReducerWithIndexes.class);
+
+            //Define type of mapper's key-value output pair
+            job.setMapOutputKeyClass(IntWritable.class);
+            job.setMapOutputValueClass(ArrayPrimitiveWritable.class);
+            
+            //Define type of reducer's key-value output pair
+            job.setOutputKeyClass(NullWritable.class);
+            job.setOutputValueClass(BloomFilter.class);
+        }
+        else { // WithBloomFilters
+            
+            //Set mapper and reducer
+            job.setMapperClass(MapRedBloomFilterMapperWithBloomFilters.class);
+            job.setReducerClass(MapRedBloomFilterReducerWithBloomFilters.class);
+
+            //Define type of mapper's key-value output pair
+            job.setMapOutputKeyClass(IntWritable.class);
+            job.setMapOutputValueClass(BloomFilter.class);
+        
+            //Define type of reducer's key-value output pair
+            job.setOutputKeyClass(IntWritable.class);
+            job.setOutputValueClass(BloomFilter.class);
+        }
+
+        //Define number of reduce tasks
+        job.setNumReduceTasks(UtilityConstants.NUM_REDUCERS);
+        
         // Config input/output
         job.setInputFormatClass(NLineInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
-        NLineInputFormat.setNumLinesPerSplit(job, 157000); // TODO: change this value     
-
+       
+        NLineInputFormat.setNumLinesPerSplit(job, numLinesPerSplit);     
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));// TODO: args or otherArgs ?
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
