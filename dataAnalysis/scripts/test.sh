fee1b5a33cd2dc8d2fe94f6c206edd5b708d0c9d
Federico Cristofani <84182920+federico-cristofani@users.noreply.github.com>
2022-06-22 19:25:09 +0200

    add test on mappers count

--- dataAnalysis/scripts/test.sh
+++ dataAnalysis/scripts/test.sh
@@ -10,9 +10,11 @@ JAR_FILE=~/it/unipi/hadoop/BloomFilter/1.0/BloomFilter-1.0.jar
 
 # Configurations
 P=(0.00001 0.0001 0.001 0.01 0.1)   ## 0.001%, 0.01%, 0.1%, 1%, 10%
-K=(0 1 3 5 7 9)                     ## K=0 means no constraints    
+K=(0 3 5 7 9)                     ## K=0 means no constraints    
 
 MAPPERS=(4 6 8 10 12)
+MAP_P=0.001
+MAP_K=0
 
 function now(){
   TZ=":Europe/Rome" date +"%d-%m-%Y_%H.%M.%S"
@@ -22,9 +24,9 @@ function launch_test(){
   i=0
   while [ $i -lt $N_BLOOM_TEST ]
   do
-    echo -en "{\"name\": \"$3P$1K$2_$i\","
-    OUT_FILE="bloom$3P$1K$2_$i"
-    INPUT="input/P$1K$2"
+    echo -en "{\"name\": \"$3P$1K$2$4_$i\","
+    OUT_FILE="bloom$3P$1K$2$4_$i"
+    INPUT="input/P$1K$2$4"
 
     start=$(date +"%s%N")
     hadoop jar $JAR_FILE it.unipi.hadoop.MapRedBloomFilter $(bash $INPUT $OUT_FILE $3)  &> .tmp
@@ -32,7 +34,7 @@ function launch_test(){
 
     echo -e "\"wallTime\":$(($stop-$start)),"
     echo -n "\"log\":\""
-    cat .tmp | tr '\t' ' ' | awk '{printf "%s\\n", $0}'
+    cat .tmp | tr '\t' ' ' | tr '"' "'" | awk '{printf "%s\\n", $0}'
 
   if [[ $i -eq $N_BLOOM_TEST-1 ]]
   then 
@@ -54,15 +56,19 @@ function launch_all(){
     for k in ${K[@]}; do 
       echo -e "\"P$p""K$k\":["
       launch_test $p $k WithBloomFilters
-      if (( $(echo "$p == ${P[-1]}" | bc -l) && $(echo "$k == ${K[-1]}" | bc -l) ))
-      then
-        echo -e "]"
-      else
-        echo -e "],"
-      fi
+      echo -e "],"
     done
   done
-  ## TODO: changing mappers
+  for m in ${MAPPERS[@]}; do
+    echo -e "\"P$P""K$K""MAP$m\":["
+    launch_test $MAP_P $MAP_K WithBloomFilters MAP$m
+    if (( $(echo "$m == ${MAPPERS[-1]}" | bc -l) ))
+    then
+      echo -e "]"
+    else
+      echo -e "],"
+    fi
+  done
   echo -e "},"
   
   
@@ -71,15 +77,19 @@ function launch_all(){
       for k in ${K[@]}; do 
         echo -e "\"P$p""K$k\":["
         launch_test $p $k WithIndexes
-        if (( $(echo "$p == ${P[-1]}" | bc -l) && $(echo "$k == ${K[-1]}" | bc -l) ))
-        then
-          echo -e "]"
-        else
-          echo -e "],"
-        fi
+        echo -e "],"
     done
   done
-  ## TODO: changing mappers
+  for m in ${MAPPERS[@]}; do
+  echo -e "\"P$P""K$K""MAP$m\":["
+  launch_test $MAP_P $MAP_K WithIndexes MAP$m
+  if (( $(echo "$m == ${MAPPERS[-1]}" | bc -l) ))
+  then
+    echo -e "]"
+  else
+    echo -e "],"
+  fi
+  done
   echo -e "}},"
   
   rm .tmp
